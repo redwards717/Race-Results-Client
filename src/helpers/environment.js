bddc773a00ef3a54bfcd80c08ca239f753688644
Rhys <redwards717@gmail.com>
2018-07-03 14:45:38 -0400

    client url change

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,6 +4,9 @@ switch (window.location.hostname) {
     case 'localhost':
         APIURL = 'http://localhost:3000';
         break;
+    case 'rce-raceresultsclient.herokuapp.com':
+        APIURL = "https://rce-raceresultsapi.herokuapp.com"
 }
 
 export default APIURL;
+
